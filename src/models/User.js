220460b9a10d1ac58c1a14bc1d2ddaaa2027cc3f
ignacioainol <iggnaxios@gmail.com>
2020-02-04 18:10:33 -0300

    second commit

--- src/models/User.js
+++ src/models/User.js
@@ -3,11 +3,11 @@ const { connecting } = require('./connect');
 const save = async (user) => {
     const connection = await connecting();
     const query = `INSERT INTO users 
-                  (user_name, user_email, user_address, user_phonenumber, user_lastname)
+                  (user_name,  user_lastname, user_email, user_address, user_phonenumber)
                   VALUES($1, $2, $3, $4, $5)
                   RETURNING *`;
     
-    const values = [user.name, user.email, user.address, user.phonenumber, user.lastname];
+    const values = [user.name, user.lastname, user.email, user.address, user.phonenumber];
     const result = await connection.query(query, values);
     connection.release();
     return result.rows[0];
